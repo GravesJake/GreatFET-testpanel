0f75e1e3d4fb7e9c5895afdeca0a03e5e68dbbea
GravesJake <jacob@greatscottgadgets.com>
2019-02-07 17:25:32 -0700

    add board state polling, update button images based on pin options set

--- testpanel.py
+++ testpanel.py
@@ -6,14 +6,19 @@ import json
 import sys
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QLabel, QPushButton, QRadioButton, QVBoxLayout
 from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, QTimer
 
 
-class MainWindow(QWidget):
+class PanelWindow(QWidget):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+        super(PanelWindow, self).__init__(parent)
     
+        hardware._init_board()
         self.init_ui()
+
+    j1_buttons = [None]
+    j2_buttons = [None]
+    j7_buttons = [None]
         
     def init_ui(self):               
         self.setWindowTitle('GreatFET Test Panel')
@@ -29,67 +34,80 @@ class MainWindow(QWidget):
         self.show()
 
     def init_buttons_ui(self):
+        # J1/J2/J7 button values
         h = 32
         w = 32
+        x = 190
         x_offset = 43
         y_offset = 41
-        self.j1_buttons = []
-        self.j2_buttons = []
-        self.j7_buttons = []
+        num_of_ports = 3
 
-        # J1/J2/J7 Buttons
-        x = 190
-        ports = 3
-        columns = 20
-        rows = 2
-
-        for i in range(ports):
-            if i == 0:
+        for port_num in range(num_of_ports):
+            if port_num == 0:
                 y = 843
-                buttons = self.j1_buttons
-            if i == 1:
+                current_buttons = PanelWindow.j1_buttons
+            elif port_num == 1:
                 y = 68
-                buttons = self.j2_buttons
-            if i == 2:
+                current_buttons = PanelWindow.j2_buttons
+            elif port_num == 2:
                 y = 112
-                buttons = self.j7_buttons
-                rows = 1
-            for j in range(columns):
-                for k in range(rows):
+                current_buttons = PanelWindow.j7_buttons
+
+            for button_pair in range(20):
+                button = QPushButton('', self)
+                button.resize(w+4,h+4)
+                button.setIcon(QIcon('icons/black_button.png'))
+                button.setIconSize(QSize(w,h))
+                button.move(x,y)
+                current_buttons.append(button)
+                if port_num != 2:
+                    y -= y_offset
                     button = QPushButton('', self)
                     button.resize(w+4,h+4)
                     button.setIcon(QIcon('icons/black_button.png'))
                     button.setIconSize(QSize(w,h))
                     button.move(x,y)
-                    buttons.append(button)
-                    y -= y_offset
-                y += y_offset*rows
+                    current_buttons.append(button)
+                    y += y_offset
                 x += x_offset
             x -= x_offset*20
 
+        PanelWindow.buttons = { 'J1': PanelWindow.j1_buttons,
+                                'J2': PanelWindow.j2_buttons,
+                                'J7': PanelWindow.j7_buttons}
+
+        self.get_board_state()
+
     def init_buttons_functions(self):
-        for i, pin_button in enumerate(self.j1_buttons, start=1):
-            if i not in hardware.j1.unclickable_pins:
+        for i, pin_button in enumerate(PanelWindow.j1_buttons):
+            if i not in hardware.j1.unclickable_pins and i != 0:
                 pin_button.clicked.connect(self.make_handle_button(hardware.j1, i))
 
-        for i, pin_button in enumerate(self.j2_buttons, start=1):
-            if i not in hardware.j2.unclickable_pins:
+        for i, pin_button in enumerate(PanelWindow.j2_buttons):
+            if i not in hardware.j2.unclickable_pins and i != 0:
                 pin_button.clicked.connect(self.make_handle_button(hardware.j2, i))
 
-        for i, pin_button in enumerate(self.j7_buttons, start=1):
-            if i not in hardware.j7.unclickable_pins:
+        for i, pin_button in enumerate(PanelWindow.j7_buttons):
+            if i not in hardware.j7.unclickable_pins and i != 0:
                 pin_button.clicked.connect(self.make_handle_button(hardware.j7, i))
 
     # factory function to handle different port/pin combinations for buttons
     def make_handle_button(self, port, pin):
         def handle_button():
-            print("handle_button")
-            print(port)
-            print(pin)
             self.dialog = OptionsWindow(port, pin)
             self.dialog.show()
         return handle_button
 
+    def get_board_state(self):
+        for port in hardware.b.ports:   # look through all the ports on the board
+            for pin in port.pins:       # look through all the pins in each port
+                if port.pins[pin].mode == "i": # i for input pins
+                    port.pins[pin].state = hardware.gf.gpio.input(port.pins[pin].tuple) # read/set the state of the input pins (High/Low)
+                    if port.pins[pin].state == True:
+                        PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/green_one_button.png'))
+                    else:
+                        PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/green_zero_button.png'))
+                
 
 class OptionsWindow(QWidget):
     def __init__(self, port, pin, parent=None):
@@ -117,15 +135,26 @@ class OptionsWindow(QWidget):
 
     def set_input(self, port, pin):
         hardware.set_input_pin(port, pin)  # configure board
+
+        # change the image on the button
+        if port.pins[pin].state == True:
+            PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/green_one_button.png'))
+        else:
+            PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/green_zero_button.png'))
     
     def set_output(self, port, pin):
         hardware.set_output_pin(port, pin)                 # configure board
+        PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/red_button.png'))
+
         
     def set_high(self, port, pin):
         hardware.set_pin_high(port, pin)
+        PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/red_one_button.png'))
 
     def set_low(self, port, pin):
         hardware.set_pin_low(port, pin)
+        PanelWindow.buttons[port.name][pin].setIcon(QIcon('icons/red_zero_button.png'))
+
 
 # this feels weird not being inside of a class and using self
 def center(self):
@@ -137,7 +166,10 @@ def center(self):
         
 def main():
     app = QApplication(sys.argv)
-    main = MainWindow()
+    panel = PanelWindow()
+    timer = QTimer()
+    timer.timeout.connect(panel.get_board_state)
+    timer.start(100)
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
